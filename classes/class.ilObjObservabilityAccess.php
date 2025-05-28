cbdf069c588f9b15ca85452558a7ac58cec10b45
LeoBouffard <leobouffard22@gmail.com>
2025-05-28 14:32:23 +0200

    ça repart

--- classes/class.ilObjObservabilityAccess.php
+++ classes/class.ilObjObservabilityAccess.php
@@ -1,7 +1,7 @@
 <?php
 class ilObjObservabilityAccess extends ilObjectPluginAccess {
 
-    public static function _checkAccess(string $cmd, string $permission, int $ref_id, int $obj_id, ?int $user_id = null): bool {
+    public function _checkAccess(string $cmd, string $permission, int $ref_id, int $obj_id, ?int $user_id = null): bool {
         return true;
     }
 
